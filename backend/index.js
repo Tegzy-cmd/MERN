14443362b67ae8df1a4ce03958ab6e25d9604116
Tegzy-cmd <77752563+Tegzy-cmd@users.noreply.github.com>
2024-04-29 17:23:34 +0100

    added CORS policy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors'
 
 
 
@@ -9,6 +10,17 @@ const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for CORS policy
+//Option 1 Allow origins with default CORS
+//Option 2 Allow custom Origin
+app.use(cors(
+  {
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type']
+  }
+))
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to MERN Stack Tutorial");
